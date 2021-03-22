b2540d7a4ab3f7853548cfbd748773faaa6f733a
Md-Aashiqq <md.aashiq.tvl@gmail.com>
2021-03-22 18:09:00 +0000

    commit first

---
+++ src/components/Header/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./style.css";
+function Header() {
+    return (
+        <div>
+            
+            <div className='header'>
+
+                <div className='left__header'>
+
+
+        
+
+<svg width="87" height="83" viewBox="0 0 87 83" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="87" height="83" fill="#21242B"/>
+<path d="M65.25 27.6666H68.875C72.7206 27.6666 76.4088 29.1241 79.128 31.7183C81.8473 34.3126 83.375 37.8311 83.375 41.5C83.375 45.1688 81.8473 48.6873 79.128 51.2816C76.4088 53.8759 72.7206 55.3333 68.875 55.3333H65.25" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M7.25 27.6666H65.25V58.7916C65.25 62.4605 63.7223 65.979 61.003 68.5733C58.2838 71.1675 54.5956 72.625 50.75 72.625H21.75C17.9044 72.625 14.2162 71.1675 11.497 68.5733C8.77767 65.979 7.25 62.4605 7.25 58.7916V27.6666Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M21.75 3.45837V13.8334" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M36.25 3.45837V13.8334" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M50.75 3.45837V13.8334" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+ 
+                    <h1 className='header__title'> Realification </h1>
+                </div>
+
+                <div className='btn__group'>
+                    <div className="signin__btn">Sign in</div>
+                    <div className="signup__btn">Sign up</div>
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
+
