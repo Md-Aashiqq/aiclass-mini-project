cda37911dee1046d38f3cb1c95eb363a2799ac41
Md-Aashiqq <md.aashiq.tvl@gmail.com>
2021-03-22 18:08:23 +0000

    initial commit

---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './style.css'
+import Header from "../../components/Header";
+function HomePage() {
+    return (
+
+        <div>
+
+            <Header />
+        
+        <div className='container'>
+           
+            <div className='left__container'>
+                <h1> Premium video meetings based on <span>   </span> </h1>
+            </div>
+            <div className='rigth__container'>
+                <h1>AShick</h1>
+            </div>
+
+        </div>
+
+        </div>
+    )
+}
+
+
+export default HomePage
